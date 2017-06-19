21307175010169e5cb7791293174f1e1a31bd087
Marcin Ruman <rumanmarcin@gmail.com>
2017-06-19 16:29:44 +0100

    Definie functional components in Preact

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import { h } from 'preact';
+import User from './User';
+
+const users = [
+    {
+        name: 'Marcin Ruman',
+        image: 'https://avatars2.githubusercontent.com/u/23522848?v=3&s=40'
+    },
+    {
+        name: 'Roberto Sobachi',
+        image: 'https://avatars3.githubusercontent.com/u/1422326?v=3&s=40'
+    }
+];
 
 export function App() {
     return (
         <div class="app">
-            <h1>Hello World</h1>
+            {users.map(user => <User {...user} key={user.name} />)}
         </div>
     );
 }
